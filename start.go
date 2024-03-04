1be661e34b272ab4a8c68052de65c79ca7671bb2
Brad Sickles <brad.sickles@gmail.com>
2024-03-04 17:20:08 -0500

    Detect AppName, Version

--- start.go
+++ start.go
@@ -1,8 +1,22 @@
 package telemetry
 
-import "context"
+import (
+	"context"
+	"os"
+)
+
+var (
+	AppName = os.Getenv("NULLSTONE_APP")
+	Version = os.Getenv("NULLSTONE_VERSION")
+)
 
 func Start(ctx context.Context, app, version string) func() {
+	if app == "" {
+		app = AppName
+	}
+	if version == "" {
+		version = Version
+	}
 	shutdownMetrics := StartMetrics(ctx, app, version)
 	shutdownTracer := StartTracer(ctx, app, version)
 	return func() {
